e00e8e5ea8fa3c60316dfbfb7c16048855465523
MariaS00 <79453713+MariaS00@users.noreply.github.com>
2021-11-04 16:43:56 +0000

    Thymeleaf works (empty new Task)

--- src/main/java/com/example/planner/controller/ReadPlannerController.java
+++ src/main/java/com/example/planner/controller/ReadPlannerController.java
@@ -3,55 +3,28 @@ package com.example.planner.controller;
 import com.example.planner.model.Category;
 import com.example.planner.model.Priority;
 import com.example.planner.model.Task;
-import com.example.planner.service.TaskService;
-import com.example.planner.service.dto.TaskView;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 //@RequestMapping("/api/planner")
 public class ReadPlannerController {
 
-    @NonNull
-    private final TaskService taskService;
+//    @NonNull
+//    private final TaskService taskService;
 
-//    @GetMapping("/tasks")
-//    public String getAll(Model model) {
-//        Task task1 = taskService.createTask("Wysłać email",
-//                LocalDate.of(2021,11,14),
-//                Priority.MEDIUM,
-//                Category.OTHER,
-//                "do: janN33@gmail.com");
-//
-//        Task task2 = taskService.createTask("Odebrać paczke",
-//                LocalDate.of(2021,11,01),
-//                Priority.LOW,
-//                Category.WORK,
-//                "adres...");
-//
-//        Task task3 = taskService.createTask("Spotkanie z Martą",
-//                LocalDate.of(2021,11,22),
-//                Priority.HIGH,
-//                Category.SOCIAL_LIFE,
-//                "Zabrać prezent!");
-//
-//        List<TaskView> tasks = taskService.getAllTasks();
+    private List<Task> tasks;
 
-//        model.addAttribute("tasks",tasks);
-//        return "main";
-//    }
-
-    @GetMapping("/tasks")
-    public String getAll(Model model) {
+    public ReadPlannerController() {
         Task task1 = new Task("Wysłać email",
                 LocalDate.of(2021,11,14),
                 Priority.MEDIUM,
@@ -64,12 +37,22 @@ public class ReadPlannerController {
                 Category.WORK,
                 "adres...");
 
-
-        List<Task> tasks = new ArrayList<>();
+        tasks = new ArrayList<>();
         tasks.add(task1);
         tasks.add(task2);
+    }
+
+    @GetMapping("/tasks")
+    public String getAll(Model model) {
         model.addAttribute("tasks",tasks);
-        return "main";
+        model.addAttribute("newTask",new Task());
+        return "task";
+    }
+
+    @PostMapping("/add-task")
+    public String addTask(@ModelAttribute Task task){
+        tasks.add(task);
+        return "redirect:/tasks";
     }
 
 
--- src/main/java/com/example/planner/model/Task.java
+++ src/main/java/com/example/planner/model/Task.java
@@ -46,5 +46,4 @@ public class Task {
                 getDescription());
     }
 
-
 }
