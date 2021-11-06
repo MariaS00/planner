673f9332956ab5301651b565f863e7652992fa9e
MariaS00 <79453713+MariaS00@users.noreply.github.com>
2021-11-06 09:04:08 +0000

    Thymeleaf works (date to do)

--- src/main/java/com/example/planner/controller/ReadPlannerController.java
+++ src/main/java/com/example/planner/controller/ReadPlannerController.java
@@ -3,6 +3,9 @@ package com.example.planner.controller;
 import com.example.planner.model.Category;
 import com.example.planner.model.Priority;
 import com.example.planner.model.Task;
+import com.example.planner.service.TaskService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,32 +18,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 //@RequestMapping("/api/planner")
 public class ReadPlannerController {
 
-//    @NonNull
-//    private final TaskService taskService;
+    @NonNull
+    private final TaskService taskService;
 
     private List<Task> tasks;
 
-    public ReadPlannerController() {
-        Task task1 = new Task("Wysłać email",
-                LocalDate.of(2021,11,14),
-                Priority.MEDIUM,
-                Category.OTHER,
-                "do: janN33@gmail.com");
-
-        Task task2 = new Task("Odebrać paczke",
-                LocalDate.of(2021,11,01),
-                Priority.LOW,
-                Category.WORK,
-                "adres...");
-
-        tasks = new ArrayList<>();
-        tasks.add(task1);
-        tasks.add(task2);
-    }
+//    public ReadPlannerController() {
+//        Task task1 = new Task("Wysłać email",
+//                LocalDate.of(2021,11,14),
+//                Priority.MEDIUM,
+//                Category.OTHER,
+//                "do: janN33@gmail.com");
+//
+//        Task task2 = new Task("Odebrać paczke",
+//                LocalDate.of(2021,11,01),
+//                Priority.LOW,
+//                Category.WORK,
+//                "adres...");
+//
+//        tasks = new ArrayList<>();
+//        tasks.add(task1);
+//        tasks.add(task2);
+//    }
 
     @GetMapping("/tasks")
     public String getAll(Model model) {
